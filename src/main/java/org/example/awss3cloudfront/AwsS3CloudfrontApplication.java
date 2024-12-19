134b3723e31080ee786761c8bec5fdeceeb67ab6
Potato-Y <projectpotatoy@gmail.com>
2024-12-19 15:18:07 +0900

    project init

---
+++ src/main/java/org/example/awss3cloudfront/AwsS3CloudfrontApplication.java
@@ -0,0 +1,13 @@
+package org.example.awss3cloudfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3CloudfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsS3CloudfrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/awss3cloudfront/AwsS3CloudfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awss3cloudfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3CloudfrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
